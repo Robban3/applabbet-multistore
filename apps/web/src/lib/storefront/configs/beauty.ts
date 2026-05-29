aa84974232300c5d267a4ab2425b08c2509d543b
Robban3 <3jstudiodesign@gmail.com>
2026-05-29 09:39:40 +0200

    feat(storefront): add beauty theme config

---
+++ apps/web/src/lib/storefront/configs/beauty.ts
@@ -0,0 +1,10 @@
+export const beautyThemeConfig = {
+  key: 'beauty',
+  categoriesTitle: 'SHOPPA KATEGORI',
+  bestSellersTitle: 'VÅRA BÄSTSÄLJARE',
+  brandsTitle: 'Våra populära varumärken',
+  hero: {
+    title: 'Lyft din naturliga skönhet',
+    description: 'Hudvård, smink och dofter noggrant utvalda för dig.'
+  }
+};
