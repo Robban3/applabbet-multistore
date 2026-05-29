b2794276f1530679b7bf52c0b2233fc42398f12f
Robban3 <3jstudiodesign@gmail.com>
2026-05-29 09:31:51 +0200

    feat(classic): add barrel exports

---
+++ apps/web/src/components/storefront/classic/index.ts
@@ -0,0 +1,7 @@
+export * from './classic-hero';
+export * from './classic-category-grid';
+export * from './classic-product-grid';
+export * from './classic-brand-strip';
+export * from './classic-value-props';
+export * from './classic-newsletter';
+export * from './classic-storefront';
